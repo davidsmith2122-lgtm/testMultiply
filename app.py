76edf8134894545b2bc1a3e93fa6aa3cc8025ffc
davidsmith2122-lgtm <davidsmith2122@gmail.com>
2026-08-09 15:37:17 +1000

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("Simple Multiplier")
 
-x = st.number_input("Enter first number", value=0.0)
+x = st.number_input("Enter yahoo it worked number", value=0.0)
 y = st.number_input("Enter second number", value=0.0)
 
 result = x * y
